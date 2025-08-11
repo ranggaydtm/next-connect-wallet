8051221dd629b886ba9a6a0e64d94bfbf65e4fca
ranggaydtm <ranggay67@gmail.com>
2025-08-11 14:31:20 +0700

    add providers documentation

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,8 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <TonConnectUIProvider manifestUrl='https://drive.google.com/file/d/1SMl6ONELgi7axLAgB08doxtyqiTMkbiD/view?usp=drive_link'>
+    // create manifest.json file into link
+    <TonConnectUIProvider manifestUrl='https://drive.google.com/file/d/1gk4jJPxZElELSX1jqjf0lvTP80QnI0U5/view?usp=drive_link'>
       {children}
     </TonConnectUIProvider>
   );
